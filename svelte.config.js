b9572f1ff1c785c91953be4f4f1a4867cda13270
isaackd <afrmtbl@gmail.com>
2025-04-23 10:55:46 -0400

    Implement automatic builds to gh-pages branch

--- svelte.config.js
+++ svelte.config.js
@@ -13,8 +13,8 @@ const config = {
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 		adapter: adapter({
-			pages: 'docs',
-			assets: 'docs',
+			pages: 'build',
+			assets: 'build',
 			fallback: null,
 			precompress: false,
 			strict: true
