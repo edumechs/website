8e1f5d6302c6e07ec1c6b1966c7887682a3c65f9
isaackd <afrmtbl@gmail.com>
2025-07-05 02:01:20 +0000

    Deploying to gh-pages from @ edumechs/website@57c312b63031ed3c83c1dd1830d286d2db799fb1 🚀

---
+++ app/immutable/chunks/C5J2bVF5.js
@@ -0,0 +1,1 @@
+import{a1 as o,X as t,_ as c}from"./C0wO9gtz.js";function f(e){throw new Error("https://svelte.dev/e/lifecycle_outside_component")}function u(e){t===null&&f(),o(()=>{const n=c(e);if(typeof n=="function")return n})}export{u as o};
